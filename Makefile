1edd219ab9e6411b02a2bb096e911880761fb154
An Hoang <anvo.ht209@gmail.com>
2023-10-04 22:57:22 +0700

    fix: fix encrypt and decrypt

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ login:
 	curl -d '{"phone":"0123456789"}' -H "Content-Type: application/json" -X POST http://localhost:3000/v1/customer/login
 
 loginverify:
-	curl -d '{"phone":"0123456789", "otp":"457818"}' -H "Content-Type: application/json" -X PUT http://localhost:3000/v1/customer/login/verify
+	curl -d '{"phone":"0123456789", "otp":"906589"}' -H "Content-Type: application/json" -X PUT http://localhost:3000/v1/customer/login/verify
 
 profile:
 	curl -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicGhvbmUiOiIwMTIzNDU2Nzg5IiwiZWF0IjoxNjk2NTE0MTE1MDg5LCJpYXQiOjE2OTY0Mjc3MTV9.nlME_JLSRJOXSjlZiavmBBX5ERG98SAmuSiwPyqHG5s" http://localhost:3000/v1/customer/profile?customer_id=1
--- common/utils/crypto.js
+++ common/utils/crypto.js
@@ -2,19 +2,22 @@ const crypto = require('crypto');
 
 function encrypt (data, secret) {
   const iv = crypto.randomBytes(16);
+  const ivh = iv.toString('hex');
   const cipher = crypto.createCipheriv('aes-256-gcm', secret, iv);
-  let encrypted = cipher.update(data, 'utf8', 'hex');
-  encrypted += cipher.final('hex');
-  const encoded = Buffer.from(encrypted).toString('base64');
+  const encrypted = cipher.update(data, 'utf8', 'hex') + cipher.final('hex');
+  const authTag = cipher.getAuthTag().toString('hex');
+  const encryptedWithMeta = [authTag, ivh, encrypted].join('|');
+  const encoded = Buffer.from(encryptedWithMeta).toString('base64');
   return encoded;
 }
 
 function decrypt (data, secret) {
   const decoded = Buffer.from(data, 'base64').toString('utf8');
-  const iv = crypto.randomBytes(16);
+  const [authTag, ivh, encrypted] = decoded.split('|');
+  const iv = Buffer.from(ivh, 'hex');
   const decipher = crypto.createDecipheriv('aes-256-gcm', secret, iv);
-  let decrypted = decipher.update(decoded, 'hex', 'utf8');
-  decrypted += decipher.final('utf8');
+  decipher.setAuthTag(Buffer.from(authTag, 'hex'));
+  const decrypted = decipher.update(encrypted, 'hex', 'utf8') + decipher.final('utf8');
   return decrypted;
 }
 
